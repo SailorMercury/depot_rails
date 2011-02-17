77c37391eb40d22eff3d8215cf1c8a36b9689b70
SailorMercury <mizunoyukino@hotmail.com>
2011-02-17 19:15:31 +0800

    v0.07

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+	def hidden_div_if(condition, attributes = {}, &block)
+		if condition 
+			attributes["styles"] = "display: none"
+		end
+		content_tag("div", attributes, &block)
+	end
 end
