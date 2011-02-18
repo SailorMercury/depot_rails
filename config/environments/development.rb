89ec4a3884fb17e580a4c17f4705ca4aed6a4938
SailorMercury <mizunoyukino@hotmail.com>
2011-02-19 01:15:39 +0800

    e-mail function debugging

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,5 +22,9 @@ Depot::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+  
+  Depot::Application.configure do
+	config.action_mailer.delivery_method = :test
+  end
 end
 
